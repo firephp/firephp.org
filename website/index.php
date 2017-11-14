5642b317bf021cc8fbfd4592927a9c038124b3e1
Christoph Dorn <christoph@christophdorn.com>
2017-11-13 22:09:14 -0800

    new links

--- website/index.php
+++ website/index.php
@@ -2,7 +2,6 @@
 
 require('_header.php');
 
-
 ?>
 <html>
 <head>
@@ -26,36 +25,30 @@ require('_header.php');
 
 <body>
 
-    
-    <h1>Firebug Extension for AJAX Development</h1>
+    <h1>Firefox Developer Tools Web Extension for PHP Development</h1>
 
     <img src="/resources/images/FirePHP_Large_White.png" width="79" height="78" border="0">
+
+    <p>Since 2007 FirePHP enables you to log to your <b>Browser Console</b> using a simple PHP method call.</p>
     
-    <p>Since 2007 FirePHP enables you to log to your <a href="http://www.getfirebug.com/" target="_blank">Firebug</a> <a target="_blank" href="http://getfirebug.com/logging.html">Console</a> using a simple PHP method call.</p>
-    
-    <p>All data is sent via response headers and will not interfere with the content on your page.</p>
+    <p>All data is sent via HTTP headers and will not interfere with the content on your page.</p>
     <p>FirePHP is ideally suited for AJAX development where clean JSON and XML responses are required.</p>
 
     <h2>Stable Downloads</h2>
 
     <ul>
-        <li><a href="https://blog.getfirebug.com/2016/02/08/merging-firebug-into-the-built-in-firefox-developer-tools/">Multi-process Architecture</a>
+        <li>Firefox Web Extension</a>
             <ul>
-                <li>Coming Soon: FirePHP Extension for Firebug.Next (Firebug 3.0) &amp; Firefox Developer Tools</li>
-            </ul>
-        </li>
-        <li>Firefox 8+ &amp; <a href="http://getfirebug.com/downloads">Firebug 1.9+</a>
-            <ul>
-                <li><a href="https://addons.mozilla.org/en-US/firefox/addon/firephp/">Install FirePHP Extension 0.7.4 from Mozilla Add-ons</a>
-        		(<a href="https://github.com/firephp/firephp-extension">Source</a>)
-        		(<a href="https://github.com/firephp/firephp-extension/blob/master/extension/CHANGELOG.md">Changelog</a>)</li>
+                <li><a href="https://addons.mozilla.org/en-US/firefox/addon/firephp/">Install FirePHP Extension from Mozilla Add-ons</a>
+        		(<a href="https://github.com/firephp/firephp-for-firefox-devtools">Source on Github</a>)
+                (<a href="https://github.com/firephp/firephp-for-firefox-devtools/wiki">Changelog</a>)
             </ul>
         </li>
         <li>FirePHPCore Server Library
             <ul>
                 <li><a href="https://github.com/firephp/firephp-core/archive/v0.4.0.zip">Download FirePHPCore 0.4.0 (zip)</a>
                 (<a href="https://github.com/firephp/firephp-core">Source</a>)
-                (<a href="https://github.com/firephp/firephp-core/blob/master/CHANGELOG.md">Changelog</a>)
+                (<a href="https://github.com/firephp/firephp-core/wiki">Changelog</a>)
                 </li>
                 <li><code>composer require firephp/firephp-core</code> (<a href="https://packagist.org/packages/firephp/firephp-core">packagist.org</a>)</li>
             </ul>
@@ -65,6 +58,11 @@ require('_header.php');
     <h2>Legacy Downloads</h2>
 
     <ul>
+        <li>Firefox 8+ &amp; <a href="http://getfirebug.com/downloads">Firebug 1.9+</a>
+            <ul>
+                <li><a href="javascript:installExtension('0.7.4-fx');">Install FirePHP Extension 0.7.4-fx</a></li>
+            </ul>
+        </li>
         <li>Firefox 5+ &amp; <a href="http://getfirebug.com/downloads">Firebug 1.8</a>
             <ul>
                 <li><a href="javascript:installExtension('0.6.2');">Install FirePHP Extension 0.6.2</a></li>
@@ -80,17 +78,6 @@ require('_header.php');
     <hr>
     
     <p>The source code for this site can be found at <a href="https://github.com/firephp/firephp.org">github.com/firephp/firephp.org</a>.</p>
-
-
-    <script type="text/javascript">
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-    </script>
-    <script type="text/javascript">
-    var pageTracker = _gat._getTracker("UA-1180396-1");
-    pageTracker._initData();
-    pageTracker._trackPageview('{/literal}{$Page.UrchinTrackerPageURI}{literal}');
-    </script>
 
 	</body>
 </html>
