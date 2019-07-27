2b4f4909aa386c62bcc656ff8daf405eccb7a0f7
Christoph Dorn <christoph@christophdorn.com>
2019-07-26 23:55:15 -0700

    Website tweaks; closes https://github.com/firephp/firephp.org/issues/3

--- public/index.php
+++ public/index.php
@@ -5,22 +5,11 @@ require('_header.php');
 ?>
 <html>
 <head>
-    <title>FirePHP - Firefox Devtools Extension for PHP Development</title>
+    <title>FirePHP - Devtools Extension for PHP Development</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <meta name="keywords" content="php, firefox, firebug, firephp, extension, ajax, javascript, zend framework, developer, development, debug, tool" />
+    <meta name="keywords" content="php, firefox, chrome, devtools, firebug, firephp, extension, ajax, javascript, zend framework, developer, development, debug, tool" />
     <meta name="description" content="Log from PHP to the Browser Console via HTTP Response Headers." />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <script>
-        function installExtension (version) {
-            if (typeof InstallTrigger !== "undefined") {
-                var xpi = {};
-                xpi['FirePHP ' + version] = 'archives/firephp-extension-' + version;
-                InstallTrigger.install(xpi);
-            } else {
-                alert('You must be using Firefox to install FirePHP!');
-            }
-        }
-    </script>
     <style>
         BODY {
             padding: 30px;
@@ -43,7 +32,7 @@ require('_header.php');
 </head>
 
 <body>
-    <h1>Firefox Devtools Extension for PHP Development</h1>
+    <h1>Devtools Extension for PHP Development</h1>
 
     <img src="/resources/images/FirePHP_Large_White.png" width="79" height="78" border="0">
 
@@ -57,7 +46,7 @@ require('_header.php');
     <h2>Supported Browsers</h2>
 
     <ul>
-        <li><a href="https://addons.mozilla.org/en-US/firefox/addon/firephp/">Install Firefox Extension</a> & open Developer Tools. Click on <img class="tabicon" src="/resources/images/tab.png" width="83" height="31" border="0"></li>
+        <li><a href="https://addons.mozilla.org/en-US/firefox/addon/firephp/">Install <b>Firefox</b> Extension</a> & open Developer Tools. Click on <img class="tabicon" src="/resources/images/tab.png" width="83" height="31" border="0"></li>
     </ul>
 
     <p>&nbsp;</p>
@@ -71,7 +60,10 @@ require('_header.php');
 
     <p>&nbsp;</p>
     
-    <p><i>FirePHP is <a href="https://github.com/firephp/">Open Source with code on Github</a>.</i></p>
+    <p>
+        <i>FirePHP is <a href="https://github.com/firephp/">Open Source with code on Github</a>.</i><br/>
+        This demo website is available via <a href="https://hub.docker.com/r/cadorn/firephp.org/">dockerhub</a> (<a href="https://github.com/firephp/firephp.org/tree/master/public">source</a>).
+    </p>
 
 </body>
 </html>
