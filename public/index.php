77c6bc9135758b831d1dc2b535cbeac47b2a0ce7
Christoph Dorn <christoph@christophdorn.com>
2019-08-30 20:03:47 -0700

    Added Chrome Logger server libraries

--- public/index.php
+++ public/index.php
@@ -81,24 +81,36 @@ require('_header.php');
 
     <ul>
         <li>FirePHP (Wildfire based)</li>
-        <li>Chrome Logger</li>
+        <li><a href="https://craig.is/writing/chrome-logger/techspecs">Chrome Logger</a></li>
     </ul>
 
     <h2>Supported Server Libraries</h2>
 
+    <p>There are also many framework plugins out there.</p>
+
     <ul>
-        <li><span class="language">PHP</span> <a href="https://github.com/firephp/firephp-core">FirePHPCore</a> - FirePHP protocol reference implementation - <code>composer require <a href="https://packagist.org/packages/firephp/firephp-core">firephp/firephp-core</a></code></li>
-        <li><span class="language">PHP</span> <a href="https://github.com/ccampbell/chromelogger">Chrome Logger</a> - Chrome Logger protocol reference implementation - <code>composer require <a href="https://packagist.org/packages/ccampbell/chromephp">ccampbell/chromephp</a></code></li>
+        <li><span class="language">PHP</span> <a href="https://github.com/firephp/firephp-core">FirePHPCore</a> - <b>FirePHP protocol reference implementation</b> - <code>composer require <a href="https://packagist.org/packages/firephp/firephp-core">firephp/firephp-core</a></code></li>
+        <li><span class="language">PHP</span> <a href="https://github.com/ccampbell/chromelogger">Chrome Logger</a> - <b>Chrome Logger protocol reference implementation</b> - <code>composer require <a href="https://packagist.org/packages/ccampbell/chromephp">ccampbell/chromephp</a></code></li>
         <li><span class="language">C</span> <a href="https://github.com/lovesyx2012/FirePHP">github.com/lovesyx2012/FirePHP</a> - FirePHP protocol implementation as a Zend PHP Extension</li>
         <li><span class="language">C</span> <a href="https://github.com/xudianyang/firephp">github.com/xudianyang/firephp</a> - FirePHP protocol implementation as a Zend PHP Extension</li>
         <li><span class="language">Java</span> <a href="https://github.com/theone1984/FireP4j">github.com/theone1984/FireP4j</a> - FirePHP protocol implementation</li>
+        <li><span class="language">Java</span> <a href="https://code.google.com/archive/p/chromelogger4j/">code.google.com/archive/p/chromelogger4j</a> - Chrome Logger protocol implementation</li>
         <li><span class="language">Ruby</span> <a href="https://github.com/smith/firephp_rails">github.com/smith/firephp_rails</a> - FirePHP protocol implementation and Rails plugin</li>
         <li><span class="language">Ruby</span> <a href="https://github.com/bluedeep/railswithfirephp">github.com/bluedeep/railswithfirephp</a> - FirePHP protocol implementation and Rails plugin</li>
+        <li><span class="language">Ruby</span> <a href="https://github.com/cookrn/chrome_logger">github.com/cookrn/chrome_logger</a> - Chrome Logger protocol implementation as a Rack middleware</li>
         <li><span class="language">Python</span> <a href="https://github.com/schmidsi/firepy">github.com/schmidsi/firepy</a> - FirePHP protocol implementation and Django extension</li>
+        <li><span class="language">Python</span> <a href="https://github.com/ccampbell/chromelogger-python">github.com/ccampbell/chromelogger-python</a> - Chrome Logger protocol implementation</li>
         <li><span class="language">Perl</span> <a href="https://github.com/willert/firephp-perl">github.com/willert/firephp-perl</a> - FirePHP protocol implementation</li>
+        <li><span class="language">Perl</span> <a href="https://metacpan.org/pod/Web::ChromeLogger">metacpan.org/pod/Web::ChromeLogger</a> - Chrome Logger protocol implementation</li>
         <li><span class="language">Common Lisp</span> <a href="https://github.com/html/firephp">github.com/html/firephp</a> - FirePHP protocol implementation</li>
+        <li><span class="language">Node.js</span> <a href="https://github.com/yannickcr/node-chromelogger">github.com/yannickcr/node-chromelogger</a> - Chrome Logger protocol implementation</li>
+        <li><span class="language">Node.js</span> <a href="https://github.com/olahol/express-chrome-logger">github.com/olahol/express-chrome-logger</a> - Chrome Logger protocol implementation as a Express middleware</li>
+        <li><span class="language">Go</span> <a href="https://github.com/gravityblast/traffic-chromelogger">github.com/gravityblast/traffic-chromelogger</a> - Chrome Logger protocol implementation as a Traffic middleware</li>
+        <li><span class="language">.NET</span> <a href="https://github.com/ChrisMissal/chromelogger">github.com/ChrisMissal/chromelogger</a> - Chrome Logger protocol implementation</li>
+        <li><span class="language">ColdFusion</span> <a href="https://github.com/s992/chromelogger-cf">github.com/s992/chromelogger-cf</a> - Chrome Logger protocol implementation</li>
         <li><span class="language">Classic ASP</span> <a href="https://github.com/dmeagor/ClassicASP-FirePHP">github.com/dmeagor/ClassicASP-FirePHP</a> - FirePHP protocol implementation</li>        
-        <li><span class="language">Classic ASP</span> <a href="https://github.com/veritech/FireASP">github.com/veritech/FireASP</a> - FirePHP protocol implementation</li>        
+        <li><span class="language">Classic ASP</span> <a href="https://github.com/veritech/FireASP">github.com/veritech/FireASP</a> - FirePHP protocol implementation</li>
+        <li><span class="language">Classic ASP</span> <a href="https://github.com/danielmarcoto/asp2console">github.com/danielmarcoto/asp2console</a> - Chrome Logger protocol implementation</li>
     </ul>
 
     <p>&nbsp;</p>
